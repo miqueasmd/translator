d9af26bdd7b9a52c2f20b231446bf2456c60957a
MMD <miqueasmd@gmail.com>
2024-10-28 08:34:18 +0100

    Add files via upload

---
+++ languages.py
@@ -0,0 +1,61 @@
+LANGUAGES = {
+    # European Languages
+    1: {'name': 'English', 'code': 'en'},
+    2: {'name': 'Spanish', 'code': 'es'},
+    3: {'name': 'French', 'code': 'fr'},
+    4: {'name': 'German', 'code': 'de'},
+    5: {'name': 'Italian', 'code': 'it'},
+    6: {'name': 'Portuguese', 'code': 'pt'},
+    7: {'name': 'Dutch', 'code': 'nl'},
+    8: {'name': 'Polish', 'code': 'pl'},
+    9: {'name': 'Romanian', 'code': 'ro'},
+    10: {'name': 'Greek', 'code': 'el'},
+
+    # Asian Languages
+    11: {'name': 'Chinese', 'code': 'zh'},
+    12: {'name': 'Japanese', 'code': 'ja'},
+    13: {'name': 'Korean', 'code': 'ko'},
+    14: {'name': 'Vietnamese', 'code': 'vi'},
+    15: {'name': 'Thai', 'code': 'th'},
+    16: {'name': 'Hindi', 'code': 'hi'},
+    17: {'name': 'Bengali', 'code': 'bn'},
+    18: {'name': 'Turkish', 'code': 'tr'},
+    19: {'name': 'Indonesian', 'code': 'id'},
+    20: {'name': 'Malay', 'code': 'ms'},
+
+    # Slavic Languages
+    21: {'name': 'Russian', 'code': 'ru'},
+    22: {'name': 'Ukrainian', 'code': 'uk'},
+    23: {'name': 'Czech', 'code': 'cs'},
+    24: {'name': 'Slovak', 'code': 'sk'},
+    25: {'name': 'Croatian', 'code': 'hr'},
+    26: {'name': 'Serbian', 'code': 'sr'},
+    27: {'name': 'Bulgarian', 'code': 'bg'},
+    28: {'name': 'Slovenian', 'code': 'sl'},
+    29: {'name': 'Macedonian', 'code': 'mk'},
+    30: {'name': 'Belarusian', 'code': 'be'},
+
+    # Nordic and Baltic Languages
+    31: {'name': 'Swedish', 'code': 'sv'},
+    32: {'name': 'Danish', 'code': 'da'},
+    33: {'name': 'Norwegian', 'code': 'no'},
+    34: {'name': 'Finnish', 'code': 'fi'},
+    35: {'name': 'Estonian', 'code': 'et'},
+    36: {'name': 'Latvian', 'code': 'lv'},
+    37: {'name': 'Lithuanian', 'code': 'lt'},
+    38: {'name': 'Icelandic', 'code': 'is'},
+    39: {'name': 'Irish', 'code': 'ga'},
+    40: {'name': 'Welsh', 'code': 'cy'},
+
+    # Other Languages
+    41: {'name': 'Arabic', 'code': 'ar'},
+    42: {'name': 'Hebrew', 'code': 'he'},
+    43: {'name': 'Persian', 'code': 'fa'},
+    44: {'name': 'Urdu', 'code': 'ur'},
+    45: {'name': 'Swahili', 'code': 'sw'},
+    46: {'name': 'Afrikaans', 'code': 'af'},
+    47: {'name': 'Albanian', 'code': 'sq'},
+    48: {'name': 'Armenian', 'code': 'hy'},
+    49: {'name': 'Georgian', 'code': 'ka'},
+    50: {'name': 'Maltese', 'code': 'mt'}
+}
